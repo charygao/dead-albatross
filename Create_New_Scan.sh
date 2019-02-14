cfcf1ea7e0977446799dfaf6fd39d4fe6a293630
Jolyon Ansuz <jansuz@users.noreply.github.com>
2019-02-15 10:49:12 +1100

    Update Create_New_Scan.sh
    Applied error checking for an attachment and referred to the attachment within the code.

--- Create_New_Scan.sh
+++ Create_New_Scan.sh
@@ -2,13 +2,20 @@
 aKey=12345678
 sKey=abcdefgh
 
+if [ $# -eq 0 ]; then
+	echo -e "\nYou need to specify a JSON file with the scan configuration in it.\n"
+	exit 99
+else 
+	FileName=${1}
+fi
+
 # POST a new scan 
 curl --verbose 'https://cloud.tenable.com/scans' \
 	-X POST \
 	-H "X-Impersonate: username=jansuz@une.edu.au" \
 	-H "X-ApiKeys: accessKey=$aKey;secretKey=$sKey" \
 	-H "Content-Type: application/json" \
-	-d @ten_create_new_scan.json
+	-d @${FileName}
 echo
 echo "Done..."
 
